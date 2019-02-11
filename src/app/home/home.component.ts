d42cd8c74eb21ca6801e5bc784499211ab448aeb
Bogdan <tushevskuy@gmail.com>
2019-02-11 11:03:22 +0200

    initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { debounceTime } from 'rxjs/operators/debounceTime';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'seed-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+  constructor() {
+    console.log(debounceTime);
+    console.log(map); // remove map for bundle testing
+  }
+}
